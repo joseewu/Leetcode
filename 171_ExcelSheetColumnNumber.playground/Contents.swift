58310d0a02d3eefd76d09e2003f77fdc681b288e
joseewu <josee@chocolabs.com>
2018-08-21 00:28:43 +0800

    26 進位

--- 171_ExcelSheetColumnNumber.playground/Contents.swift
+++ 171_ExcelSheetColumnNumber.playground/Contents.swift
@@ -3,11 +3,17 @@
 import Foundation
 
 class Solution {
-    class Solution {
-        func titleToNumber(_ s: String) -> Int {
-
+    func titleToNumber(_ s: String) -> Int {
+        let zHash:Int = "Z".unicodeScalars.first!.hashValue
+        let aHash:Int = "A".unicodeScalars.first!.hashValue
+        var result:Int = 0
+        for (_, element) in s.unicodeScalars.enumerated() {
+            let itemHash:Int = element.hashValue
+            let difference = (zHash - aHash + 1) - (zHash - itemHash)
+            result = (result * 26) + difference
         }
+        return result
     }
 }
-
-
+let testCase = "ZY"
+let result = Solution().titleToNumber(testCase)
