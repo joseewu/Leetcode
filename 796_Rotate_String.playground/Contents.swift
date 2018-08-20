9177b9369b016ca21cdeb795f3a7a1a9f4935ff1
joseewu <josee@chocolabs.com>
2018-08-21 00:32:00 +0800

    796. Rotate String

---
+++ 796_Rotate_String.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+/*
+ We are given two strings, A and B.
+
+ A shift on A consists of taking string A and moving the leftmost character to the rightmost position. For example, if A = 'abcde', then it will be 'bcdea' after one shift on A. Return True if and only if A can become B after some number of shifts on A.
+ */
+import Foundation
+class Solution {
+    func rotateString(_ A: String, _ B: String) -> Bool {
+        return true
+    }
+}
+let A = "abcde", B = "cdeab"
+let result = Solution().rotateString(A, B)
