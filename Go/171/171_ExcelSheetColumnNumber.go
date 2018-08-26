09e2a3234b1ce368584c1584ee9728b6b8abb936
joseewu <josee@chocolabs.com>
2018-08-27 01:25:41 +0800

    finish 171 in go

--- Go/171/171_ExcelSheetColumnNumber.go
+++ Go/171/171_ExcelSheetColumnNumber.go
@@ -11,7 +11,12 @@ func main()  {
   fmt.Println(result)
 }
 func titleToNumber(s string) int {
-    // fmt.Println(string(s[0])) just print the character
-    fmt.Println(s[0]) //print unicode of the character
-    return 0
+    runes := []rune(s)
+    AValue := []rune("A")[0]
+    result := 0
+    for _, runCharacter := range runes {
+      differece := runCharacter - AValue + 1
+      result = (result * 26) + int(differece)
+    }
+    return result
 }
